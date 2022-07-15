74732131664c8a201ccde890d221da23676a1d7e
Dhiraj Bhavsar <dheeraj.89@live.com>
2022-07-15 10:50:16 +0530

    Add project files.

---
+++ Mdrx.Hub.Common/MapHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mdrx.Hub.Common
+{
+    public class MapHelper
+    {
+        public static string GenerateGuid()
+        {
+            return Guid.NewGuid().ToString();
+        }
+
+        public static string ConvertToDateTime(string dateAsString)
+        {
+            return Convert.ToDateTime(dateAsString).ToString("O");
+        }
+    }
+}
---
+++ Mdrx.Hub.Common/RuleEngineHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mdrx.Hub.Common
+{
+    public class RuleEngineHelper
+    {
+        public RuleEngineHelper()
+        {
+
+        }
+        public static bool IsDeclined(float orderTotal) => orderTotal > 5000;
+    }
+}
